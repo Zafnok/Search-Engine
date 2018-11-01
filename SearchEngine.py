4176be8d58f5d654c743e5aa439a6b238298615b
Nick Wentz <zafnok98@gmail.com>
2018-11-01 09:07:16 -0400

    Update SearchEngine.py

--- SearchEngine.py
+++ SearchEngine.py
@@ -4,8 +4,9 @@ import inflect
 
 # TODO check variable names, some are bad
 # TODO add NOT/AND/OR
+# TODO add functionality to write on close
 class SearchEngine:
-    searchDictionary = {}
+    search_dictionary = {}
     inflect_engine = inflect.engine()
 
     @staticmethod
@@ -14,25 +15,28 @@ class SearchEngine:
 
     def add_to_dictionary(self, tags, data):
         for tag in tags:
-            if tag in self.searchDictionary:
-                self.searchDictionary[tag].add(data)
+            if tag in self.search_dictionary:
+                self.search_dictionary[tag].add(data)
             else:
-                self.searchDictionary[tag] = {data}
+                self.search_dictionary[tag] = {data}
 
     def search_keys(self, user_input):
         results = {}
-        results = self.create_results_set(results, user_input)
-        results = self.order_dictionary(results)
-        if len(results) == 0:
-            new_tag_str = input("Search returned zero results\nWhat were you searching for?\nType "
-                                "None if you don't want to add to the engine.\n")
-            if not new_tag_str.lower().strip() == "none":
-                self.add_to_dictionary(user_input.strip().split(" "), new_tag_str)
+        if len(user_input.strip()) > 0:
+            results = self.create_results_set(results, user_input)
+            results = self.order_dictionary(results)
+            if len(results) == 0:
+                new_tag_str = input("Search returned zero results\nWhat were you searching for?\nType "
+                                    "None if you don't want to add to the engine.\n")
+                if not new_tag_str.lower().strip() == "none":
+                    self.add_to_dictionary(user_input.strip().split(" "), new_tag_str)
+            else:
+                return_str = ""
+                for pair in results:
+                    return_str += (pair[0] + " - hits: " + str(pair[1]) + "\n")
+                return return_str
         else:
-            return_str = ""
-            for pair in results:
-                return_str += (pair[0] + " - hits: " + str(pair[1]) + "\n")
-            return return_str
+            return "Invalid input, please try again."
 
     # TODO create function to make this more concise
 
@@ -46,11 +50,11 @@ class SearchEngine:
                     last_word = last_word[1:]
                     remove_flag = True
             except ValueError:
-                last_word = user_input[0:len(user_input)-1]
+                last_word = user_input[0:len(user_input) - 1]
                 if last_word[0] == '-':
                     remove_flag = True
                     last_word = last_word[1:]
-            for key in self.searchDictionary.keys():
+            for key in self.search_dictionary.keys():
                 if key.startswith(last_word):
                     self.send_to_helper(results_set, exclude_from_results_set, key, remove_flag)
             last_index_of_space = user_input.rfind(" ")
@@ -62,14 +66,14 @@ class SearchEngine:
             if word[0] == '-':
                 remove_flag = True
                 word = word[1:]
-            if word in self.searchDictionary.keys():
+            if word in self.search_dictionary.keys():
                 self.send_to_helper(results_set, exclude_from_results_set, key, remove_flag)
             else:
                 plural_str = self.inflect_engine.plural(word)
                 singular_str = self.inflect_engine.singular_noun(word)
-                if plural_str != word and plural_str in self.searchDictionary.keys():
+                if plural_str != word and plural_str in self.search_dictionary.keys():
                     self.send_to_helper(results_set, exclude_from_results_set, plural_str, remove_flag)
-                elif singular_str != word and singular_str in self.searchDictionary.keys():
+                elif singular_str != word and singular_str in self.search_dictionary.keys():
                     self.send_to_helper(results_set, exclude_from_results_set, singular_str, remove_flag)
         return dict(results_set.items() - exclude_from_results_set.items())
 
@@ -80,7 +84,7 @@ class SearchEngine:
             self.create_results_set_helper(results_set, key)
 
     def create_results_set_helper(self, results_set, key):
-        for tag in self.searchDictionary[key]:
+        for tag in self.search_dictionary[key]:
             if tag in results_set:
                 results_set[tag] = results_set[tag] + 1
             else:
@@ -88,22 +92,22 @@ class SearchEngine:
 
     def write_to_file(self, file_name):
         try:
-            with open(file_name, 'x+') as dataFile:
-                for key, value in self.searchDictionary.items():
-                    dataFile.write(key + " ")
-                    dataFile.write(", ".join(value))
-                    dataFile.write("\n")
+            with open(file_name, 'x+') as data_file:
+                self.write_helper(data_file)
         except FileExistsError:
-            with open(file_name, 'w') as dataFile:
-                for key, value in self.searchDictionary.items():
-                    dataFile.write(key + " ")
-                    dataFile.write(", ".join(value))
-                    dataFile.write("\n")
+            with open(file_name, 'w') as data_file:
+                self.write_helper(data_file)
+
+    def write_helper(self, data_file):
+        for key, value in self.search_dictionary.items():
+            data_file.write(key + " ")
+            data_file.write(", ".join(value))
+            data_file.write("\n")
 
     def populate_from_file(self, file_name):
         try:
-            with open(file_name, 'r') as dataFile:
-                for line in dataFile:
+            with open(file_name, 'r') as data_file:
+                for line in data_file:
                     line = line.strip("\n")
                     self.add_to_dictionary({line[0:line.index(" ")]}, line[line.index(" ") + 1:])
         except FileNotFoundError:
